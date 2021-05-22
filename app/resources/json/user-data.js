0a9233693fd8e347be8dbd49c3cf2f298dfaf429
Saravana Kumar <development.ravanar@gmail.com>
2021-05-22 11:34:54 +0530

    finished home pahe

---
+++ app/resources/json/user-data.js
@@ -0,0 +1,24 @@
+const userData = {
+    "name": {
+        "first": "Saravana",
+        "middle": "Kumar",
+        "last": "K R"
+    },
+    "jobTitles": [
+        "iOS Developer",
+        "React Native Developer",
+        "Android Hobbyist",
+        "Web Hobbyist",
+        "Freelancer"
+    ],
+    "about_me": "I'm a Mobile Application Developer based out of Kerala, India. I am skilled in iOS, Android platform app developments with naive framework and with cross platform framework(React Native)",
+    "birthday": "05.12.1992",
+    "age": 31,
+    "address": "Kollam, Kerala, India - 691021",
+    "email": "saravanakumar0512@gmail.com",
+    "phone": [
+        "+918921234687",
+        "+919633519779"
+    ],
+    "study": "Kerala University"
+}
