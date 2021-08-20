f8811ca446371ae3193ae90ad99812712abf7285
Saravana Kumar <development.ravanar@gmail.com>
2021-08-20 21:28:20 +0530

    Updated home page animation

--- app/scripts/home-page.js
+++ app/scripts/home-page.js
@@ -2,22 +2,24 @@
 document.addEventListener('DOMContentLoaded', domContentLoaded)
 function domContentLoaded() {
 
-    homePageAboutMe.innerHTML = userData.jobTitles[0]
+    homePageAboutMe.innerHTML = `${userData.jobTitles[0]}  `
+
+    homePageAboutMe.style.animationName = "home-page-about-me-width-animation"
 }
 
 let homePageAboutMe = document.getElementById('home-page-about-me');
-let indexJobTitle = 1
-let indexHomePageAboutMeAnimationStatus = 0
+let indexJobTitle = 0; // Current 
+let indexHomePageAboutMeAnimationStatus = 0;
 homePageAboutMe.addEventListener('animationstart', (ev) => {
     console.log("Animation Startted", ev);
 });
-
 homePageAboutMe.addEventListener('animationiteration', (ev) => {
     console.log("animation_iteration", ev);
     ++indexHomePageAboutMeAnimationStatus
     if ((indexHomePageAboutMeAnimationStatus % 2) === 1) {
-        homePageAboutMe.innerHTML = userData.jobTitles[indexJobTitle % userData.jobTitles.length]
         ++indexJobTitle
+        homePageAboutMe.innerHTML = `${userData.jobTitles[indexJobTitle % userData.jobTitles.length]}  ` 
+        // ++indexJobTitle
     }
 });
 
